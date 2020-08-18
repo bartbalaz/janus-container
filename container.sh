509d0124bb9c6da2a0a1c461d151de25e8b70874
Bartosz Balazinski <bartosz.balazinski@nuance.com>
2020-08-18 12:49:07 -0400

    Initial commit

--- container.sh
+++ container.sh
@@ -1,19 +1,15 @@
 #! /bin/bash
 
-create_dir() {
-   if [ ! -d "$1" ]; then
-      mkdir -p $1
-   fi
-}
+# Parameters
 
-ROOT_DIR=$(pwd)/root
-STAGING_DIR=$(pwd)/staging
+SCRIPT_DIR=$(pwd)
+ROOT_DIR=$SCRIPT_DIR/root
+STAGING_DIR=$SCRIPT_DIR/staging
 JANUS_DIR=$ROOT_DIR/janus
-
-
-create_dir $ROOT_DIR
-create_dir $STAGING_DIR
-
+JANUS_CONFIG_DIR=$SCRIPT_DIR/janus_config
+IMAGE_NAME=janus
+IMAGE_VERSION=01
+FULL_IMAGE_NAME=$IMAGENAME:$IMAGE_VERSION
 
 #copy_requirement() {
 #   if [ ! -d "$(dirname $ROOT_DIR$(which $1))" ]; then
@@ -23,44 +19,110 @@ create_dir $STAGING_DIR
 #}
 
 
-# libnice installation
-
-cd $STAGING_DIR
-git clone https://gitlab.freedesktop.org/libnice/libnice
-cd libnice
-meson --prefix=$ROOT_DIR/usr build && ninja -C build &&  ninja -C build install 
-
-# libsrtp installation
-
-cd $STAGING_DIR
-wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
-tar xfv v2.2.0.tar.gz
-cd libsrtp-2.2.0
-./configure --prefix=$ROOT_DIR/usr --enable-openssl
-make shared_library && make install
+create_dir() {
+   if [ ! -d "$1" ]; then
+      mkdir -p $1
+   fi
+}
 
 
-# janus-gateway installation
+create() {
 
-cd $STAGING_DIR
-git clone https://github.com/bartbalaz/janus-gateway.git
-cd janus-gateway
-git checkout Nuance.0.0.3
-/bin/bash $(pwd)/autogen.sh
-/bin/bash $(pwd)/configure --prefix=$JANUS_DIR --enable-post-processing
-make
-make install
-make configs
-# Remove all the unecessary files and folders (not usefull in a container context)
-rm -rf $JANUS_DIR/include
-rm -rf $JANUS_DIR/share
-# Empty the default configuration folder
-rm $JANUS_DIR/etc/janus/*
-# Copy the container configuraiton
-cp $ROOT_DIR/../janus_config/* $JANUS_DIR/etc/janus/
+	echo "Creating root and staging directories"
+	echo "-------------------------------------"
+	
+	create_dir $ROOT_DIR
+	create_dir $STAGING_DIR
+
+	echo "Installing libnice (latest avaialble version)"
+	echo "---------------------------------------------"
+
+	cd $STAGING_DIR
+	git clone https://gitlab.freedesktop.org/libnice/libnice
+	cd libnice
+	meson --prefix=$ROOT_DIR/usr build && ninja -C build &&  ninja -C build install 
+
+	echo "Installing libsrtp-2.2.0"
+	echo "------------------------"
+	
+	cd $STAGING_DIR
+	wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+	tar xfv v2.2.0.tar.gz
+	cd libsrtp-2.2.0
+	./configure --prefix=$ROOT_DIR/usr --enable-openssl
+	make shared_library && make install
 
 
-# Create additional directories for mounting the platform certificates maintained by certbot
+	echo "Installing janus-gateway Nuance.0.0.3 version"
+	echo "---------------------------------------------"
 
-create_dir $ROOT_DIR/etc/certs
-create_dir $ROOT_DIR/archive
+	cd $STAGING_DIR
+	git clone https://github.com/bartbalaz/janus-gateway.git
+	cd janus-gateway
+	git checkout Nuance.0.0.3
+	/bin/bash $(pwd)/autogen.sh
+	/bin/bash $(pwd)/configure --prefix=$JANUS_DIR --enable-post-processing
+	make
+	make install
+	make configs
+	# Remove all the unecessary files and folders (not usefull in a container context)
+	rm -rf $JANUS_DIR/include
+	rm -rf $JANUS_DIR/share
+	# Empty the default configuration folder
+	rm $JANUS_DIR/etc/janus/*
+	# Copy the container configuraiton
+	cp $ROOT_DIR/../$JANUS_CONFIG_DIR/* $JANUS_DIR/etc/janus/
+
+
+	echo "Creating directory for mounting the certbot certificates"
+	echo "--------------------------------------------------------"
+
+	create_dir $ROOT_DIR/etc/certs
+	create_dir $ROOT_DIR/archive
+}
+
+build() {
+	echo "Building docker image into local repository"
+	echo "-------------------------------------------"
+	
+	docker rmi -f $FULL_IMAGE_NAME
+
+	docker build -t $FULL_IMAGE_NAME .
+}
+
+clean() {
+	echo "Cleaning"
+	echo "--------"
+	
+	rm -rf $STAGING_DIR
+	rm -rf $ROOT_DIR
+}
+
+launch() {
+	echo "Launching in interactive mode"
+	echo "-----------------------------"
+
+	docker run -it  -p 8089:8089 -p 8088:8088 -p 7889:7889 -v /etc/letsencrypt/live/bart-janus-02.eastus.cloudapp.azure.com:/etc/certs -v /etc/letsencrypt/archive:/archive janus
+
+}
+
+
+for arg in "$@" 
+do
+	case $arg in 
+		create) 
+			create
+			;;
+		build)
+			build
+			;;
+		clean)
+			clean
+			;;
+		launch)
+			launch
+	esac
+done
+
+
+
