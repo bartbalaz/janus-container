5efd69362e174af24dc504ce7c6fa43183912142
Balazinski <bartosz.balazinski@nuance.com>
2020-09-10 17:06:45 -0400

    Updating setup instructions

--- container.sh
+++ container.sh
@@ -94,10 +94,10 @@ create() {
 	[ ! -z "$JANUS_VERSION" ] && git checkout $JANUS_VERSION
 	/bin/bash $(pwd)/autogen.sh
 	export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$ROOT_DIR/usr/lib/pkgconfig:$ROOT_DIR/usr/lib/x86_64-linux-gnu/pkgconfig
-	export CFLAGS=$CFLAGS -I$ROOT_DIR/usr/include
-	export CPPFLAGS=$CFLAGS -I$ROOT_DIR/usr/include
-
 	/bin/bash $(pwd)/configure --with-sysroot=$ROOT_DIR --prefix=$JANUS_DST_DIR --enable-post-processing
+	export CFLAGS=-I$ROOT_DIR/usr/include
+	export CPPFLAGS=-I$ROOT_DIR/usr/include
+
 	#--with-sysroot=$ROOT_DIR
 	make
 	make install
