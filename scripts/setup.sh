cc2732a7007c261c0a013c207673e346a8970e46
Balazinski <bartosz.balazinski@nuance.com>
2020-09-14 21:53:52 -0400

    Updating setup instructions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+echo "Running $0"
+
 echo "Step 1 - Installing the prerequisites"
+echo "-------------------------------------"
+echo
 apt update
 apt install git
 apt install -y build-essential
 
 echo "Step 2 - Installing docker"
+echo "--------------------------"
+echo
 apt update
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -14,6 +20,8 @@ apt update
 apt install docker-ce docker-ce-cli containerd.io
 
 echo "Step 3 - Installing the build requirements"
+echo "------------------------------------------"
+echo
 apt update
 apt install -y python3-pip libmicrohttpd-dev libavutil-dev libavcodec-dev libavformat-dev libogg-dev libcurl4-openssl-dev libconfig-dev libjansson-dev libglib2.0-dev libssl-dev build-essential graphviz default-jdk flex bison cmake libtool automake liblua5.3-dev pkg-config gengetopt 
 pip3 install meson
