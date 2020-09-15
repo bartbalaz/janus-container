6064e57b84a2117169afdaf9319723f8d12ea3a7
Balazinski <bartosz.balazinski@nuance.com>
2020-09-14 21:51:01 -0400

    Updating setup instructions

--- scripts/build.sh
+++ scripts/build.sh
@@ -12,7 +12,7 @@
 # environments where the returned values may not be appropriate 
 
 # Global variables - Should not need to be modified
-TOP_DIR=$(pwd)
+TOP_DIR=/
 
 ROOT_DIR=$TOP_DIR/root
 STAGING_DIR=$TOP_DIR/staging
@@ -64,87 +64,86 @@ test_parameter() {
 	fi
 }
 
-create() {
-	test_parameter JANUS_REPO $JANUS_REPO optional
-	test_parameter JANUS_REPO $JANUS_VERSION optional
 
-	echo "Creating root and staging directories"
-	echo "-------------------------------------"
-	
-	create_dir $ROOT_DIR
-	create_dir $STAGING_DIR
+test_parameter JANUS_REPO $JANUS_REPO optional
+test_parameter JANUS_REPO $JANUS_VERSION optional
 
-	echo "Installing libnice (latest avaialble version)"
-	echo "---------------------------------------------"
+echo "Creating root and staging directories"
+echo "-------------------------------------"
 
-	cd $STAGING_DIR
-	git clone https://gitlab.freedesktop.org/libnice/libnice
-	cd libnice
-	meson --prefix=$ROOT_DIR/usr build && ninja -C build &&  ninja -C build install 
+create_dir $ROOT_DIR
+create_dir $STAGING_DIR
 
-	echo "Installing libsrtp-2.2.0"
-	echo "------------------------"
-	
-	cd $STAGING_DIR
-	wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
-	tar xfv v2.2.0.tar.gz
-	cd libsrtp-2.2.0
-	./configure --prefix=$ROOT_DIR/usr --enable-openssl
-	make shared_library && make install
+echo "Installing libnice (latest avaialble version)"
+echo "---------------------------------------------"
 
-	echo "Installing janus-gateway"
-	echo "---------------------------------------------"
-	cd $STAGING_DIR
-	if [ -z "$JANUS_REPO" ]
-	then 
-		echo "Cloning from default repo to $JANUS_CLONE_DIR"
-		git clone https://github.com/meetecho/janus-gateway.git $JANUS_CLONE_DIR
-	else
-		echo "Cloning from $JANUS_REPO to $JANUS_CLONE_DIR"
-		git clone $JANUS_REPO $JANUS_CLONE_DIR
-	fi
-	cd $JANUS_CLONE_DIR
-	[ ! -z "$JANUS_VERSION" ] && git checkout $JANUS_VERSION
-	/bin/bash $(pwd)/autogen.sh
-	
-	export PKG_CONFIG_PATH=$ROOT_DIR/usr/lib/pkgconfig:$ROOT_DIR/usr/lib/x86_64-linux-gnu/pkgconfig 
-	/bin/bash $(pwd)/configure --prefix=$JANUS_DST_DIR CFLAGS=-I$ROOT_DIR/usr/include 
-	 
-	make
-	make install
-	make configs 
-	
-	echo "Removing include and share directories"
-	echo "--------------------------------------------------------"
-	purge_dir $JANUS_DST_INCLUDE_DIR
-	purge_dir $JANUS_DST_SHARE_DIR
-	
-	echo "Removing default configuration"
-	echo "--------------------------------------------------------"
-	purge_dir $JANUS_DST_CONFIG_DIR
-	
-	echo "Copying custop configuration"
-	echo "--------------------------------------------------------"
-	mkdir $JANUS_DST_CONFIG_DIR
-	cp $JANUS_SRC_CONFIG_DIR/* $JANUS_DST_CONFIG_DIR
-	
-	echo "Copying the Janus HTML examples"
-	echo "--------------------------------------------------------"
-	create_dir $JANUS_DST_HTML_DIR
-	create_dir $JANUS_DST_HTML_MOUNT_DIR
-	cp -R $JANUS_SRC_HTML_DIR/* $JANUS_DST_HTML_DIR
-	
-	echo "Creating directory for mounting the certbot certificates"
-	echo "--------------------------------------------------------"
-	create_dir $CERTIFICATE_LINKS_DIR
-	create_dir $CERTIFICATE_ARCHIVE_DIR
+cd $STAGING_DIR
+git clone https://gitlab.freedesktop.org/libnice/libnice
+cd libnice
+meson --prefix=$ROOT_DIR/usr build && ninja -C build &&  ninja -C build install 
 
-	echo "Creating directory for saving the recordings"
-	echo "--------------------------------------------------------"
-	createdir $JANUS_DST_RECORDING_DIR
-	
-	echo "Copying the startup script into the root directory"
-	echo "--------------------------------------------------------"
-	cp $START_SCRIPT_SRC $START_SCRIPT_DST
-	chmod a+x $START_SCRIPT_DST
-}
+echo "Installing libsrtp-2.2.0"
+echo "------------------------"
+
+cd $STAGING_DIR
+wget https://github.com/cisco/libsrtp/archive/v2.2.0.tar.gz
+tar xfv v2.2.0.tar.gz
+cd libsrtp-2.2.0
+./configure --prefix=$ROOT_DIR/usr --enable-openssl
+make shared_library && make install
+
+echo "Installing janus-gateway"
+echo "---------------------------------------------"
+cd $STAGING_DIR
+if [ -z "$JANUS_REPO" ]
+then 
+	echo "Cloning from default repo to $JANUS_CLONE_DIR"
+	git clone https://github.com/meetecho/janus-gateway.git $JANUS_CLONE_DIR
+else
+	echo "Cloning from $JANUS_REPO to $JANUS_CLONE_DIR"
+	git clone $JANUS_REPO $JANUS_CLONE_DIR
+fi
+cd $JANUS_CLONE_DIR
+[ ! -z "$JANUS_VERSION" ] && git checkout $JANUS_VERSION
+/bin/bash $(pwd)/autogen.sh
+
+export PKG_CONFIG_PATH=$ROOT_DIR/usr/lib/pkgconfig:$ROOT_DIR/usr/lib/x86_64-linux-gnu/pkgconfig 
+/bin/bash $(pwd)/configure --prefix=$JANUS_DST_DIR CFLAGS=-I$ROOT_DIR/usr/include 
+ 
+make
+make install
+make configs 
+
+echo "Removing include and share directories"
+echo "--------------------------------------------------------"
+purge_dir $JANUS_DST_INCLUDE_DIR
+purge_dir $JANUS_DST_SHARE_DIR
+
+echo "Removing default configuration"
+echo "--------------------------------------------------------"
+purge_dir $JANUS_DST_CONFIG_DIR
+
+echo "Copying custop configuration"
+echo "--------------------------------------------------------"
+mkdir $JANUS_DST_CONFIG_DIR
+cp $JANUS_SRC_CONFIG_DIR/* $JANUS_DST_CONFIG_DIR
+
+echo "Copying the Janus HTML examples"
+echo "--------------------------------------------------------"
+create_dir $JANUS_DST_HTML_DIR
+create_dir $JANUS_DST_HTML_MOUNT_DIR
+cp -R $JANUS_SRC_HTML_DIR/* $JANUS_DST_HTML_DIR
+
+echo "Creating directory for mounting the certbot certificates"
+echo "--------------------------------------------------------"
+create_dir $CERTIFICATE_LINKS_DIR
+create_dir $CERTIFICATE_ARCHIVE_DIR
+
+echo "Creating directory for saving the recordings"
+echo "--------------------------------------------------------"
+createdir $JANUS_DST_RECORDING_DIR
+
+echo "Copying the startup script into the root directory"
+echo "--------------------------------------------------------"
+cp $START_SCRIPT_SRC $START_SCRIPT_DST
+chmod a+x $START_SCRIPT_DST
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
 
-# This script cretes the Janus build image.
-# This script isntalls Docker binaries but uses the host docker engine via the docker socket
-# by running: docker run -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock
-
-# Step 1 -  Install the basic pre-requisites
+echo "Step 1 - Installing the prerequisites"
 apt update
 apt install git
-apt install build-essential
+apt install -y build-essential
 
-# Step 2 - Install docker
+echo "Step 2 - Installing docker"
 apt update
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
@@ -17,7 +13,7 @@ add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(
 apt update
 apt install docker-ce docker-ce-cli containerd.io
 
-# Step 3 - Install buld requirements
+echo "Step 3 - Installing the build requirements"
 apt update
 apt install -y python3-pip libmicrohttpd-dev libavutil-dev libavcodec-dev libavformat-dev libogg-dev libcurl4-openssl-dev libconfig-dev libjansson-dev libglib2.0-dev libssl-dev build-essential graphviz default-jdk flex bison cmake libtool automake liblua5.3-dev pkg-config gengetopt 
 pip3 install meson
