622f159eb737a770f541d96ab5b2a322a2f24ca7
Balazinski <bartosz.balazinski@nuance.com>
2020-09-15 15:30:08 -0400

    Updating setup instructions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -9,15 +9,15 @@ echo "***************************"
 echo
 
 echo
-echo "Step 1 - Installing the prerequisites and convenience packages"
-echo "--------------------------------------------------------------"
+echo " Step 1 - Installing the prerequisites and convenience packages "
+echo "----------------------------------------------------------------"
 echo
 apt update
 DEBIAN_FRONTEND="noninteractive" apt install -y apt-utils build-essential tree wget vim
 
 echo
-echo "Step 2 - Installing docker"
-echo "--------------------------"
+echo " Step 2 - Installing docker "
+echo "----------------------------"
 echo
 apt update
 DEBIAN_FRONTEND="noninteractive" apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
@@ -27,8 +27,8 @@ apt update
 DEBIAN_FRONTEND="noninteractive" apt install -y docker-ce docker-ce-cli containerd.io
 
 echo
-echo "Step 3 - Installing the build requirements"
-echo "------------------------------------------"
+echo " Step 3 - Installing the build requirements "
+echo "--------------------------------------------"
 echo
 apt update
 DEBIAN_FRONTEND="noninteractive" apt install -y python3-pip libmicrohttpd-dev libavutil-dev libavcodec-dev libavformat-dev libogg-dev libcurl4-openssl-dev libconfig-dev libjansson-dev libglib2.0-dev libssl-dev build-essential graphviz default-jdk flex bison cmake libtool automake liblua5.3-dev pkg-config gengetopt 
