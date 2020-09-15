73532f974f113b73aef47a1f1411a044f3fd3ff1
Balazinski <bartosz.balazinski@nuance.com>
2020-09-14 21:37:20 -0400

    Updating setup instructions

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -5,14 +5,15 @@
 # by running: docker run -p 8080:8080 -v /var/run/docker.sock:/var/run/docker.sock
 
 # Step 1 -  Install the basic pre-requisites
+apt update
 apt install git
 apt install build-essential
 
 # Step 2 - Install docker
 apt update
 apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 apt update
 apt install docker-ce docker-ce-cli containerd.io
 
