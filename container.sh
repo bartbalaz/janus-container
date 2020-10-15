868e0305e380f3699a4bb4b78eacea05f9820831
Balazinski <bartosz.balazinski@nuance.com>
2020-10-14 22:17:35 -0400

    Periodic commit

--- container.sh
+++ container.sh
@@ -94,7 +94,9 @@ else
 	echo 
 	echo "Creating the build image using $IMAGE_TOOL"
 	echo "--------------------------------------"
-	$IMAGE_TOOL build --build-arg IMAGE_TOOL=$IMAGE_TOOL -t $FULL_BUILD_IMAGE_NAME -f Dockerfile.build . 
+	$IMAGE_TOOL build  -t $FULL_BUILD_IMAGE_NAME -f Dockerfile.build . 
+
+#--build-arg IMAGE_TOOL=$IMAGE_TOOL
 
 	if [ ! -z $IMAGE_REGISTRY ]; then 
 		# We need to push the image to registry
