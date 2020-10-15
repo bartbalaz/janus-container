69b6e55aa6d765b87056b4cac8627e775ff8631b
Balazinski <bartosz.balazinski@nuance.com>
2020-10-14 23:16:39 -0400

    Periodic commit

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -45,7 +45,7 @@ else
 	echo "deb https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/ /" | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:stable.list
 	curl -L https://download.opensuse.org/repositories/devel:/kubic:/libcontainers:/stable/xUbuntu_${VERSION_ID}/Release.key | apt-key add -
 	apt update
-	DEBIAN_FRONTEND="noninteractive" apt -y upgrade 
+	# DEBIAN_FRONTEND="noninteractive" apt -y upgrade 
 	DEBIAN_FRONTEND="noninteractive" apt -y install podman
 fi
 
